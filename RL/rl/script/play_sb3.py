1aecb41066981999e256e5482a39cb84476bd0fb
Yu-Tang Liu <yutang.liu@tuebingen.mpg.de>
2021-09-30 14:20:59 +0200

    play_sb3 with args

--- RL/rl/script/play_sb3.py
+++ RL/rl/script/play_sb3.py
@@ -1,10 +1,11 @@
+import argparse
 from blimp_env.envs import PlanarNavigateEnv
 from blimp_env.envs.common.gazebo_connection import GazeboConnection
 
 from blimp_env.envs.script import close_simulation
 from sb3_contrib import QRDQN
 
-
+# args
 ENV = PlanarNavigateEnv
 AGENT = QRDQN
 
@@ -12,28 +13,44 @@ AGENT = QRDQN
 time_steps = 100000
 
 # model
-model_path = "RL/rl/trained_model/final_model.zip"
+default_model_path = "RL/rl/trained_model/final_model.zip"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gui", type=bool, default=True, help="Start with gazebo gui")
+parser.add_argument("--enable_meshes", type=bool, default=True, help="with meshes")
+parser.add_argument(
+    "--model_path", type=str, default=default_model_path, help="path to policy model"
+)
+parser.add_argument(
+    "--time_steps",
+    type=int,
+    default=time_steps,
+    help="time steps for testing, 0.5s/step",
+)
+parser.add_argument(
+    "--task", type=str, default="square", help="square or hover_fixed_goal"
+)
+args = parser.parse_args()
 
 # environments"""
 env_kwargs = {
     "DBG": True,
     "seed": 123,
-    "real_experiment": False,
     "simulation": {
         "robot_id": str(0),
         "ros_port": 11311,
         "gaz_port": 11351,
-        "gui": True,
-        "enable_meshes": True,
+        "gui": args.gui,
+        "enable_meshes": args.enable_meshes,
         "world": "basic",
-        "task": "square",  # hover_fixed_goal
+        "task": args.task,
         "auto_start_simulation": True,
         "update_robotID_on_workerID": True,
     },
     "observation": {
         "DBG_OBS": True,
     },
-    "success_threshhold": 0.1,
+    "success_threshhold": 0.075,
 }
 
 
@@ -55,4 +72,4 @@ def play(env_kwargs, model_path, time_steps):
         GazeboConnection().unpause_sim()
 
 
-play(env_kwargs=env_kwargs, model_path=model_path, time_steps=time_steps)
+play(env_kwargs=env_kwargs, model_path=args.model_path, time_steps=args.time_steps)
