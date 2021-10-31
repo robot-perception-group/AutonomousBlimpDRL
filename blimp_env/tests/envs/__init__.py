58d0be985baffe507ae232646ccffc1fbbf1d07c
Yu-Tang Liu <yutang.liu@tuebingen.mpg.de>
2021-10-31 16:59:27 +0100

    clear env test

--- blimp_env/tests/envs/__init__.py
+++ blimp_env/tests/envs/__init__.py
@@ -1,4 +1,4 @@
 from blimp_env.envs.script import spawn_simulation_for_testing, close_simulation
 
-# close_simulation()
-# spawn_simulation_for_testing(1, gui=False)
+close_simulation()
+spawn_simulation_for_testing(1, gui=False)
