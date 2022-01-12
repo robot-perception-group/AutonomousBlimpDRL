89b53d4c5237b201f9d33f74ffa5d9fc8b0e3c6b
Yu-Tang Liu <yutang.liu@tuebingen.mpg.de>
2022-01-12 16:46:48 +0100

    update

---
+++ RL/rl/rllib_script/util.py
@@ -0,0 +1,10 @@
+def find_nearest_power_of_two(a):
+    """return closest power of two number smaller than a
+    i.e. if a = 1025, return 1024
+
+    """
+    i = 0
+    while a > 1:
+        a = a / 2
+        i += 1
+    return 2 ** (i - 1)
