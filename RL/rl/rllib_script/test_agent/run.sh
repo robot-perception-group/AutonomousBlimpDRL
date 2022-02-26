8b2e1a76105b15b8cc9d98f208753c369a2e5bc3
Yu-Tang Liu <yutang.liu@tuebingen.mpg.de>
2022-02-26 22:22:50 +0100

    update

--- RL/rl/rllib_script/test_agent/run.sh
+++ RL/rl/rllib_script/test_agent/run.sh
@@ -1,31 +1,34 @@
 #/bin/bash
 
-expname=$1
-auto_start_simulation=$2
-
 script_full_path=$(dirname "$0")
 
+for ((i=0; i<=1; i++)); do
+  for ((j=0; j<=2; j++)); do
+    expname=agent_$i_$j
+    trajecotry=$i
+    windspeed=$j
 
-if [[ $auto_start_simulation -eq 1 ]] 
-then
-  bash $script_full_path/cleanup.sh
-fi
+    bash $script_full_path/cleanup.sh
+
+    echo "start test agent"
+    screen -d -m -S TestAgent screen bash -c "\
+      source ~/catkin_ws/devel/setup.bash;\
+      python3 $script_full_path/test_agent.py $trajecotry $windspeed;"
 
 
-echo "start test agent"
-screen -d -m -S TestAgent screen bash -c "\
-  source ~/catkin_ws/devel/setup.bash;\
-  python3 $script_full_path/test_agent.py $auto_start_simulation;"
+    echo "wait for environment to wake up"
+    sleep 280;
 
+    echo "start recording"
+    screen -d -m -S RECORD bash -i $script_full_path/start_parallel_record.sh "$expname"
 
-echo "wait for environment to wake up"
-sleep 280;
+    echo "record experiment for 15min (30min in total due to 2x speed in gazebo)"
+    sleep 900; 
 
-echo "start recording"
-screen -d -m -S RECORD bash -i $script_full_path/start_parallel_record.sh "$expname"
+    echo "finish recording"
+    screen -d -m -S RECORD bash -i $script_full_path/killbag.sh 
 
-echo "record experiment for 15min (30min in total due to 2x speed in gazebo)"
-sleep 900; 
+  done
+done
 
-echo "finish recording"
-screen -d -m -S RECORD bash -i $script_full_path/killbag.sh 
+echo "finish all experiments"
