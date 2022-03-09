07d6e92fce72e672a3451e72ee38649282045e4b
Yu-Tang Liu <yutang.liu@tuebingen.mpg.de>
2022-03-09 16:04:26 +0100

    update readme

--- RL/rl/rllib_script/test_agent/run.sh
+++ RL/rl/rllib_script/test_agent/run.sh
@@ -1,32 +1,44 @@
 #/bin/bash
 
 script_full_path=$(dirname "$0")
+LOGDIR=${HOME}/ray_results/Robustness/rosbag
 
-for ((i=1; i<=4; i++)); do
-  expname=agent_adaptive_${i}
-  run_pid=$i
+for ((i=0; i<=1; i++)); do
+  for ((j=0; j<=2; j++)); do
+    for ((k=0; k<=2; k++)); do
+      for ((l=0; l<=1; l++)); do
 
-  bash $script_full_path/cleanup.sh
+        expname=evaluation_${i}_${j}_${k}_${l}
+        run_pid=$i
+        wind=$j
+        buoyancy=$k
+        trajectory=$l
 
-  echo "start test agent"
-  screen -d -m -S TestAgent screen bash -c "\
-    source ~/catkin_ws/devel/setup.bash;\
-    python3 $script_full_path/test_agent.py "$run_pid" ;"
+
+        bash $script_full_path/cleanup.sh
+
+        echo "start test agent"
+        screen -d -m -S TestAgent screen bash -c "\
+          source ~/catkin_ws/devel/setup.bash;\
+          python3 $script_full_path/test_agent.py "$run_pid" "$wind" "$buoyancy" "$trajectory" ;"
 
 
-  echo "wait for environment to wake up"
-  sleep 280;
+        echo "wait for environment to wake up"
+        sleep 285;
 
-  #echo "start recording"
-  #screen -d -m -S RECORD bash -i $script_full_path/start_parallel_record.sh "$expname"
+        echo "start recording"
+        screen -d -m -S RECORD bash -i $script_full_path/start_parallel_record.sh "$expname" "$LOGDIR"
 
-  #echo "record experiment for 15min"
-  echo "wait for 1.5hr"
-  sleep 5400; 
+        echo "record experiment for 15min (which is 30min with gazebo double speed)"
+        sleep 900;
 
-  #echo "finish recording"
-  #screen -d -m -S RECORD bash -i $script_full_path/killbag.sh 
-  #sleep 10;
+        echo "finish recording"
+        screen -d -m -S RECORD bash -i $script_full_path/killbag.sh 
+        sleep 10;
+
+      done
+    done
+  done
 done
 
 bash $script_full_path/cleanup.sh
